f18ea3073870ba12aa015e24cd64d52f8e1d90c8
unknown <daniel_rieping@hotmail.com>
2021-11-16 23:21:05 -0300

    chore: deploy Hiroku

--- src/middlewares/authenticated.ts
+++ src/middlewares/authenticated.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response } from "express";
-import { verify } from "jsonwebtoken";
-import authConfig from "../config/auth";
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
 
 interface Token {
     sub: string;
     role: string;
 }
 
-export default function authenticated(request:Request, response:Response, next: NextFunction){
+export default function authenticated(request: Request, response:Response, next:NextFunction) {
 
     const headerAuthorization = request.headers.authorization
 
@@ -16,22 +17,22 @@ export default function authenticated(request:Request, response:Response, next: 
             error: "Token not found"
         })
     }
-
     const [, token] = headerAuthorization.split(' ');
-
-    const verifyToken = verify(token,authConfig.jwt.secret);
+      
+    const verifyToken = verify(token, authConfig.jwt.secret);
 
     if(!verifyToken){
         throw new Error();
     }
-    
-    const {sub, role} = verifyToken as Token;
 
+    const {sub, role} = verifyToken as Token
+
+    
     request.body.user = {
         id: sub,
         role: role
     }
-
+    
     return next();
-
+  
 }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import './dotenv/config';
+import 'dotenv/config';
 import express from 'express';
+import './database';
 import cors from 'cors';
 import routes from './routes';
-import './database';
-import config from './config/server'
-
-
+import config from './config/server';
 
 const app = express();
 const port = config.server.port;
@@ -14,5 +12,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-/* Teste */
+
 app.listen(port, () => console.log('Server started'));
